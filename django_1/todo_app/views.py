e25985e58e0d0a794aa2b5f4c2db176a5d6c12fb
sean <seanschmidt89@yahoo.com>
2022-05-02 00:57:16 -0700

    updating django proj

--- django_1/todo_app/views.py
+++ django_1/todo_app/views.py
@@ -1,14 +1,23 @@
-from msilib.schema import CreateFolder
-from django.shortcuts import render
+from .models import Task
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView
-from .models import Task
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
+
+from django.contrib.auth.views import LoginView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 # Create your views here.
 
-class TaskList(ListView):
+class CustomLoginView(LoginView):
+    template_name = 'todo_app/login.html'
+    fields = '__all__'
+    redirect_authenticated_user = True
+
+    def get_success_url(self):
+        return reverse_lazy('tasks')
+
+class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
     template_name = 'todo_app/index.html'
@@ -30,4 +39,10 @@ class TaskUpdate(UpdateView):
     fields = '__all__'
     template_name = 'todo_app/task_form.html'
     success_url = reverse_lazy('tasks')
+
+class TaskDelete(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
+    template_name = 'todo_app/delete.html'
  
