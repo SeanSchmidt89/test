003a6327ba2f00d8272c0c482ddd4781a3e5059b
sean <seanschmidt89@yahoo.com>
2022-05-07 04:33:28 -0700

    updating todo_app

--- django_1/todo_app/views.py
+++ django_1/todo_app/views.py
@@ -48,6 +48,13 @@ class TaskList(LoginRequiredMixin, ListView):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
         context['count'] = context['tasks'].filter(complete=False).count()
+
+        search_input = self.request.GET.get('search-area') or ''
+        if search_input:
+            context['tasks'] = context['tasks'].filter(title__startswith=search_input)
+
+        context['search_input'] = search_input
+
         return context
 
 
@@ -60,7 +67,7 @@ class TaskDetail(LoginRequiredMixin, DetailView):
 class TaskCreate(LoginRequiredMixin, CreateView):
     model = Task
     fields = ['title', 'description', 'complete']
-    template_name = 'todo_app/task_form.html'
+    template_name = 'todo_app/create.html'
     success_url = reverse_lazy('tasks')
     
     def form_valid(self, form):
@@ -71,7 +78,7 @@ class TaskCreate(LoginRequiredMixin, CreateView):
 class TaskUpdate(LoginRequiredMixin, UpdateView):
     model = Task
     fields = ['title', 'description', 'complete']
-    template_name = 'todo_app/task_form.html'
+    template_name = 'todo_app/task_update.html'
     success_url = reverse_lazy('tasks')
 
 
