665c9624927aed6d99b274f84651322391366762
sean <seanschmidt89@yahoo.com>
2022-05-09 00:34:59 -0700

    updating weather app

--- django_weather/weather_app/views.py
+++ django_weather/weather_app/views.py
@@ -1,12 +1,34 @@
 from django.shortcuts import render
 import requests
+import datetime
 
 # Create your views here.
 
 def index(request):
+    if 'city' in request.POST:
+        city = request.POST['city']
+
+    else:
+        city = 'eugene'
     api_key = '5630a17a3869f6cedeb147809fc2f514'
-    api_url = 'https://api.openweathermap.org/data/2.5/weather?q={city name}&appid='+api_key
+    api_url = 'https://api.openweathermap.org/data/2.5/weather?q='+city+'&appid='+api_key
     api_data = requests.get(api_url)
     api_json = api_data.json()
-    context = {}
-    return render(request, 'weather_app/index.html')
+    descrition = api_json['weather'][0]['description']
+    temp = api_json['main']['temp']
+    humidity = api_json['main']['humidity']
+    wind = api_json['wind']['speed']
+    icon = api_json['weather'][0]['icon']
+    day = datetime.date.today()
+    context = {
+        'descrition': descrition,
+        'temp': temp,
+        'humidity': humidity,
+        'wind': wind,
+        'icon': icon,
+        'day': day,
+        'city': city,
+    }
+    return render(request, 'weather_app/index.html', context)           
+
+                        
