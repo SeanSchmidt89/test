c7280cfe83a08dd3426ad5ea68a0e01308c2411d
sean <seanschmidt89@yahoo.com>
2022-05-29 06:17:31 -0700

    updating blog2

--- blog2/blog_app/urls.py
+++ blog2/blog_app/urls.py
@@ -4,4 +4,7 @@ from .views import *
 urlpatterns = [
     path('', IndexList.as_view(), name='index'),
     path('update/<int:pk>/', PostUpdate.as_view(), name='update'),
+    path('delete/<int:pk>', PostDelete.as_view(), name='delete'),
+    path('addpost', AddPost.as_view(), name='addpost'),
+    path('postdetails/<int:pk>', PostDetails.as_view(), name='postdetails'),
 ]
--- blog2/blog_app/views.py
+++ blog2/blog_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import DeleteView, ListView, UpdateView, DetailView
+from django.views.generic import DeleteView, ListView, UpdateView, DetailView, CreateView
 
 from blog_app.models import Post
 
@@ -19,3 +19,23 @@ class PostUpdate(UpdateView):
     template_name = 'blog_app/update.html'
     fields = '__all__'
     success_url = reverse_lazy('index')
+
+
+class PostDelete(DeleteView):
+    model = Post
+    template_name = 'blog_app/delete.html'
+    success_url = reverse_lazy('index')
+
+
+class AddPost(CreateView):
+    model = Post
+    template_name = 'blog_app/addpost.html'
+    fields = '__all__'
+    success_url = reverse_lazy('index')
+
+
+class PostDetails(DetailView):
+    model = Post
+    fields = "__all__"
+    template_name = 'blog_app/postdetails.html'
+    
