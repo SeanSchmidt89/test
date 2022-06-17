3b237bf5d000312916bb4f38e448f213a9ed4e98
sean <seanschmidt89@yahoo.com>
2022-06-16 18:35:42 -0700

    adding serialzers page

--- vue/todo_app/urls.py
+++ vue/todo_app/urls.py
@@ -5,4 +5,5 @@ from .import views
 app_name = 'todo_app'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('list/', views.todo_list, name='todo_list'),
 ]
--- vue/todo_app/views.py
+++ vue/todo_app/views.py
@@ -1,8 +1,27 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import *
+from .serializer import TodoSerializer
 
 # Create your views here.
 
 def index(request):
     return render(request, 'todo_app/index.html')
+
+
+
+@api_view(['GET'])
+def todo_list(request):
+    response = Response()
+
+    todos = Todo.objects.all()
+    todo_serializer = TodoSerializer(todos, many=True)
+
+    response.data = {
+        'todos': todo_serializer.data
+    }
+
+    return response
 
 #39:00 - 43:00 back end
