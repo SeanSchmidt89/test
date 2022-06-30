ecbf73147e8b0de5c848e1863e7714cb2e8b9650
sean <seanschmidt89@yahoo.com>
2022-06-30 01:33:05 -0700

    adding for loops for Vue

--- vue/main.js
+++ vue/main.js
@@ -1,12 +1,31 @@
 const app = Vue.createApp({
     data(){
-        return{
-            message: 'hello world',
-            num: 5,
-            img: 'https://www.topgear.com/sites/default/files/news-listicle/image/2020/03/1993-toyota-supra-twin-turbo-sport-roof-_0.jpg?w=1290&h=726',
-            link: 'https://vuejs.org',
+        return {
+            users: [
+                {
+                    id: 567,
+                    name: 'bill',
+                    profession: 'developer'
+                },
+                {
+                    id: 567,
+                    name: 'tim',
+                    profession: 'chief'
+                },
+                {
+                    id: 567,
+                    name: 'phill',
+                    profession: 'plumber'
+                },
+                {
+                    id: 567,
+                    name: 'batman',
+                    profession: 'batman'
+                },
+            ],
         }
     }
 })
 
-const mountedApp = app.mount('#app');
+const mountedApp = app.mount('#app')
+
