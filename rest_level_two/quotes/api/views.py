26e47591664fcd021831e1a8784a5fc46637d113
sean <seanschmidt89@yahoo.com>
2022-06-30 22:33:06 -0700

    finishing project

--- rest_level_two/quotes/api/views.py
+++ rest_level_two/quotes/api/views.py
@@ -1,12 +1,17 @@
-from quotes.models import Quote
 from rest_framework import serializers, generics
+from quotes.models import Quote
+from quotes.api.permissions import IsAdminUserOrReadOnly
 from .serializers import QuoteSerializer
+
+
 
 class QuoteListCreateAPIView(generics.ListCreateAPIView):
     queryset = Quote.objects.all().order_by('-id')
     serializer_class = QuoteSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
 
 
 class QuoteDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Quote.objects.all().order_by('-id')
+    queryset = Quote.objects.all()
     serializer_class = QuoteSerializer
+    permission_classes = [IsAdminUserOrReadOnly]
--- rest_level_two/quotesapi/settings.py
+++ rest_level_two/quotesapi/settings.py
@@ -123,3 +123,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+REST_FRAMEWORK = {
+    'PAGE_SIZE': 30,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+}
