48a3076309dbe46949231eceaf2b145dbfcfe0f2
sean <seanschmidt89@yahoo.com>
2022-07-12 23:15:39 -0700

    adding models, base page, views

--- django_practice/app/models.py
+++ django_practice/app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=50)
+    content = models.CharField(max_length=255)
+    created = models.DateField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def str(self):
+        return self.title
--- django_practice/app/views.py
+++ django_practice/app/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Todo
 
 # Create your views here.
 
 def index(request):
-    return render(request, "app/index.html")
+    todos = Todo.objects.all()
+    context = {
+        'todos': todos,
+    }
+    return render(request, "app/index.html", context)
