c621b2f84d01d88c5f106867f39cdc0d1df02aa4
sean <seanschmidt89@yahoo.com>
2022-07-18 23:47:04 -0700

    adding crud pages and views

--- django_practice/app/views.py
+++ django_practice/app/views.py
@@ -9,3 +9,20 @@ def index(request):
         'todos': todos,
     }
     return render(request, "app/index.html", context)
+
+
+def create(request, pk):
+    pass
+
+
+
+def update(request, pk):
+    pass
+
+
+def delete(request, pk):
+    pass
+
+
+def detail(request, pk):
+    pass
