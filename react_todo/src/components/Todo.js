713e772b2873dd7ac6d5997342f0adcaa4aa0e7c
sean <seanschmidt89@yahoo.com>
2022-07-27 06:40:05 -0700

    adding new youtube proj

--- react_todo/src/components/Todo.js
+++
@@ -1,9 +0,0 @@
-import React, { useState } from "react";
-import TodoForm from "./TodoForm";
-
-const Todo = () => {
-  const [edit, setEdit] = useState({ id: null });
-  return <div></div>;
-};
-
-export default Todo;
