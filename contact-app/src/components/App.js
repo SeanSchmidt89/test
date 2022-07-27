2342cdb30c0b9fd7a794521f0d168226d2a8e42d
sean <seanschmidt89@yahoo.com>
2022-07-27 08:08:19 -0700

    adding components

--- contact-app/src/components/App.js
+++ contact-app/src/components/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header";
+import AddContact from "./AddContact";
+import ContactList from "./ContactList";
 
 function App() {
+  const contact = [
+    {
+      id: "1",
+      name: "sean",
+      email: "sean@blahblah.com",
+    },
+    {
+      id: "2",
+      name: "tim",
+      email: "tim@blahblah.com",
+    },
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header />
+      <AddContact />
+      <ContactList contacts={contact} />
     </div>
   );
 }
