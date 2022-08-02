3d82bdaab76919fd58bd3e44a2140dec386a6e2e
sean <seanschmidt89@yahoo.com>
2022-08-02 02:06:07 -0700

    adding new react proj

---
+++ react_todo/src/App.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import "./App.css";
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todo, setTodo] = useState("");
+
+  const inputHandler = (event) => {
+    setTodo(event.target.value);
+  };
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo,
+      completed: false,
+    };
+    setTodos([...todos, newTodo]);
+    setTodo("");
+  };
+
+  const deleteHandler = (id) => {
+    const updatedTodos = [...todos].filter((todo) => todo.id !== id)
+    setTodos(updatedTodos)
+  }
+
+  return (
+    <div className="App">
+      <h1>Todo App</h1>
+      <br />
+      <form onSubmit={submitHandler}>
+        <input type="text" onChange={inputHandler} value={todo} />
+        <button type="submit">Add Todo</button>
+      </form>
+      {todos.map((todo) => 
+        <div key={todo.id}>
+          <div>{todo.text}</div>
+          <button onClick={() => deleteHandler(todo.id)}>Delete</button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default App;
