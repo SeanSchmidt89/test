98057d2fd7b4010851d0f796c5679e03df0c0205
sean <seanschmidt89@yahoo.com>
2022-08-03 08:45:52 -0700

    addind css to components

---
+++ react_user/src/App.js
@@ -0,0 +1,13 @@
+import AddUser from './components/Users/AddUser';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      <AddUser />
+    </div>
+  );
+}
+
+export default App;
