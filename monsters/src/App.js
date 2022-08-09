79235af67adad16c58a527c76067d9464bfa3b5c
sean <seanschmidt89@yahoo.com>
2022-08-08 17:16:34 -0700

    adding filters

--- monsters/src/App.js
+++ monsters/src/App.js
@@ -7,22 +7,45 @@ class App extends Component {
 
     this.state = {
       monsters: [],
+      searchField: "",
     };
   }
 
-
   componentDidMount() {
-    fetch('https://jsonplaceholder.typicode.com/users')
+    fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((users) => this.setState(() => {
-        return {monsters: users}
-      }))
+      .then((users) =>
+        this.setState(() => {
+          return { monsters: users };
+        })
+      );
+  }
+
+
+  onSearchChange = (event) => {
+    const searchField = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchField };
+    });
   }
 
   render() {
+    const { monsters, searchField} = this.state;
+    const { onSearchChange } = this;
+
+    const filteredMonsters = monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(searchField);
+    });
+
     return (
       <div className="App">
-        {this.state.monsters.map((monster) => (
+        <input
+          className="search-box"
+          type="search"
+          placeholder="Search"
+          onChange={onSearchChange}
+        />
+        {filteredMonsters.map((monster) => (
           <p key={monster.id}>{monster.name}</p>
         ))}
       </div>
---
+++ pract_react/src/components/Main.js
@@ -0,0 +1,52 @@
+import { Component } from "react";
+import React from "react";
+
+class Main extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      users: [
+        { id: Math.random().toString(), name: "bill" },
+        { id: Math.random().toString(), name: "tim" },
+        { id: Math.random().toString(), name: "cleater" },
+      ],
+      searchUser: "",
+    };
+  }
+
+  inputChange = (event) => {
+    //getting value, lowercasing it and saving it to state
+    const searchUser = event.target.value.toLocaleLowerCase();
+    this.setState(() => {
+      return { searchUser };
+    });
+  };
+
+  render() {
+    //filtering usersList vs searchUser list, saving in filteredUseres
+    const filteredUsers = this.state.users.filter((user) => {
+      return user.name.toLocaleLowerCase().includes(this.state.searchUser)}
+    );
+
+
+    //in return map over filtered list
+    return (
+      <div>
+        <h1>HI</h1>
+        <input
+          onChange={this.inputChange}
+          type="search"
+          placeholder="Search User"
+        />
+        <button type="submit">Search</button>
+        
+        {filteredUsers.map((user) => (
+          <p key={user.id}>{user.name}</p>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Main;
