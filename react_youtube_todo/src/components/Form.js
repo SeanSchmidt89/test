1c06a4713476d9223a3a4442f031db5b76a4a73d
sean <seanschmidt89@yahoo.com>
2022-08-21 23:09:21 -0700

    deleting proj

--- react_youtube_todo/src/components/Form.js
+++ react_youtube_todo/src/components/Form.js
@@ -14,7 +14,6 @@ const Form = ({ inputText, setInputText, setTodos, todos }) => {
     };
     setTodos((prevList) => [...prevList, todo]);
     setInputText("");
-    console.log(todos);
   };
 
   return (
