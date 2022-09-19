7f34b1295126aadf7c4444f790b8a23eaa73d29e
sean <seanschmidt89@yahoo.com>
2022-09-18 22:45:08 -0700

    adding redux 2

---
+++ react-redux-two/src/App.js
@@ -0,0 +1,13 @@
+import Counter from "./components/Counter";
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
