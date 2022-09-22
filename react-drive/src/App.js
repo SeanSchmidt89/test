256e050c1d3cd2c9db1ee193513f4e0466599bb8
sean <seanschmidt89@yahoo.com>
2022-09-22 05:44:24 -0700

    updaing proj

--- react-drive/src/App.js
+++ react-drive/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import NavBar from "./components/navbar/NavBar";
+import Hero from "./components/hero/Hero";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Hero />
     </div>
   );
 }
---
+++ react-netflix/src/App.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import MovieDetail from "./components/MovieDetail/MovieDetail";
+import PageNotFound from "./components/PageNotFound/PageNotFound";
+import "./App.scss";
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/movie/:imdbID" element={<MovieDetail />} />
+        <Route path='pagenotfound' element={<PageNotFound />} />
+      </Routes>
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
+
+// key: fd73ac94
