d984d14e4c3d1853d872c53d13fa433e0a202fd1
sean <seanschmidt89@yahoo.com>
2022-10-05 08:53:11 -0700

    added photos

--- redux-netflix-axios/src/components/Header/Header.js
+++ redux-netflix-axios/src/components/Header/Header.js
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
+import user from "../../images/user.png";
 
 const Header = () => {
   return (
-    <div>Header</div>
-  )
-}
+    <div className="header">
+      <Link to="/">
+        <div className="logo">Movie App</div>
+      </Link>
+      <div className="user-image">
+        <img src={user} alt="user" />
+      </div>
+    </div>
+  );
+};
 
-export default Header
+export default Header;
