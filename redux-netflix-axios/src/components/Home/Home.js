2a21496d1ea522e8ca3a0e1304ae789c30c369ec
sean <seanschmidt89@yahoo.com>
2022-10-08 01:33:28 -0700

    adding logic to handle api data

--- redux-netflix-axios/src/components/Home/Home.js
+++ redux-netflix-axios/src/components/Home/Home.js
@@ -2,22 +2,23 @@ import React, { useEffect } from "react";
 import MovieListing from "../MovieListing/MovieListing";
 import movieApi from "../../common/apis/movieApi";
 import { APIkey } from "../../common/apis//MovieApiKey";
+import { useDispatch } from "react-redux";
+import { addMovies } from "../../store/movieSlice";
 
 const Home = () => {
+  const dispatch = useDispatch();
+  const movieText = "Harry";
   useEffect(() => {
-    const movieText = "Harry";
-
     const fetchMovies = async () => {
       const response = await movieApi
         .get(`?apikey=${APIkey}&s=${movieText}&type=movie`)
         .catch((err) => {
           console.log("error: ", err);
         });
-      console.log("response: ", response);
+      dispatch(addMovies(response.data));
     };
-
     fetchMovies();
-  }, []);
+  }, [dispatch]);
   return (
     <div className="home">
       <div className="banner-img">
