641d1277389e26668d2e61fa2dfc2ae7a39778d0
sean <seanschmidt89@yahoo.com>
2022-10-09 22:03:46 -0700

    adding styling to moive card

--- redux-netflix-axios/src/components/MovieCard/MovieCard.js
+++ redux-netflix-axios/src/components/MovieCard/MovieCard.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import "./MovieCard.scss";
 
-const MovieCard = (props) => {
+const MovieCard = ({ data }) => {
   return (
-    <div>MovieCard</div>
-  )
-}
+    <div className="card-item">
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data.Poster} alt={data.title} />
+        </div>
+        <div className="card-bottom">
+          <div className="card-info"><p>{data.Title}</p></div>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MovieCard
+export default MovieCard;
