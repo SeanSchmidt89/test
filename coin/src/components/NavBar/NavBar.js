410f0855c036e7110b5ea260ad730a020de9a46d
sean <seanschmidt89@yahoo.com>
2022-10-28 04:19:16 -0700

    adding new proj, added navbar

---
+++ coin/src/components/NavBar/NavBar.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {FaBars, FaTimes} from "react-icons/fa"
+import "./NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="header">
+      <div className="container">
+        <h1>
+          De<span className="primary">Fi</span>
+        </h1>
+        <ul className="nav-menu">
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/">Featured</a>
+          </li>
+          <li>
+            <a href="/">Earn</a>
+          </li>
+          <li>
+            <a href="/">Contact</a>
+          </li>
+        </ul>
+        <div className="btn-group">
+          <button className="btn">Connect Wallet</button>
+        </div>
+        <div className="hamburger">
+            <FaBars size={20} style={{color: '#333'}}/>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
