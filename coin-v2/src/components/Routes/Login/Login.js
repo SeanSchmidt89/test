e0ce1bd41c378a7749e46398a109fde88f889e4f
sean <seanschmidt89@yahoo.com>
2022-11-10 08:35:03 -0800

    finished sign in comp css and context with firebase

--- coin-v2/src/components/Routes/Login/Login.js
+++ coin-v2/src/components/Routes/Login/Login.js
@@ -1,8 +1,28 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="login">
+      <div className="container">
+        <div className="form-container">
+          <h2>Login</h2>
+          <form className="login-form">
+            <input type="email" placeholder="email" />
+            <input type="password" placeholder="password" />
+            <button type="submit">Login</button>
+          </form>
+          <p>
+            Dont have an account?{" "}
+            <Link to="/signup">
+              <span>SignUp</span>
+            </Link>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
--- coin-v2/src/components/Routes/SignUp/SignUp.js
+++ coin-v2/src/components/Routes/SignUp/SignUp.js
@@ -1,17 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { UserAuth } from "../../../context/AuthContext";
 import "./SignUp.css";
 
 const SignUp = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const { user, signUp } = UserAuth();
+  const navigate = useNavigate();
+
+  const formHandler = async (e) => {
+    e.preventDefault();
+    try {
+      await signUp(email, password);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+    }
+    setEmail("");
+    setPassword("");
+  };
   return (
     <div className="signup">
       <div className="container">
         <div className="form-container">
           <h2>Sign Up</h2>
-          <form className="sign-in-form">
-            <input type="email" placeholder="email" />
-            <input type="password" placeholder="password" />
+          <form onSubmit={formHandler} className="sign-in-form">
+            <input
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+              type="email"
+              placeholder="email"
+            />
+            <input
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+              type="password"
+              placeholder="password"
+            />
             <button type="submit">Sign Up</button>
           </form>
+          <p>
+            Already have an account?{" "}
+            <Link to="/login">
+              <span>login</span>
+            </Link>
+          </p>
         </div>
       </div>
     </div>
