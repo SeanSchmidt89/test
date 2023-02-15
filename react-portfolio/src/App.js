05e51198f81edebb18c915ac70628d39320a8e84
sean <seanschmidt89@yahoo.com>
2023-02-14 23:18:50 -0800

    updating new projct with comps folder and react-icons

---
+++ react-portfolio/src/App.js
@@ -0,0 +1,12 @@
+import SideNav from "./components/SIdeNav/SideNav";
+
+function App() {
+  return (
+    <div className="App">
+      hi
+      <SideNav />
+    </div>
+  );
+}
+
+export default App;
---
+++ react-portfolio/src/components/SIdeNav/SideNav.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { AiOutLineMenu } from "react-icons/ai";
+
+const SideNav = () => {
+  return (
+    <div>
+      <AiOutLineMenu />
+    </div>
+  );
+};
+
+export default SideNav;
