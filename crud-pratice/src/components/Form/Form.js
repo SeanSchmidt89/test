29db707e7aca0982cc16ce3c7cee7e693f9e027a
sean <seanschmidt89@yahoo.com>
2023-03-01 01:21:24 -0800

    adding css to todo and priority to form

--- crud-pratice/src/components/Form/Form.js
+++ crud-pratice/src/components/Form/Form.js
@@ -3,13 +3,21 @@ import TodoContext from "../../TodoContext";
 import "./Form.css";
 
 const Form = () => {
-  const { todos, title, titleHandler, formHandler } = useContext(TodoContext);
+  const { title, titleHandler, formHandler } = useContext(TodoContext);
   return (
     <div className="form">
       <form onSubmit={formHandler}>
         <div>
           <label>Title</label>
           <input onChange={titleHandler} value={title} />
+        </div>
+        <div>
+          <label>Priority</label>
+          <select name='priority'>
+            <option value="high">High</option>
+            <option value="medium">Medium</option>
+            <option value="low">low</option>
+          </select>
         </div>
         <button type="submit">Add Todo</button>
       </form>
--- crud-pratice/src/components/Todo/Todo.js
+++ crud-pratice/src/components/Todo/Todo.js
@@ -1,8 +1,10 @@
-import React, { useContext } from "react";
+import React, { useState, useContext } from "react";
 import TodoContext from "../../TodoContext";
 import "./Todo.css";
 
 const Todo = ({ item }) => {
+  const [edit, setEdit] = useState(false);
+  const [editText, setEditText] = useState(item.title);
   const { todos, setTodos } = useContext(TodoContext);
 
   const deleteHandler = () => {
@@ -20,12 +22,44 @@ const Todo = ({ item }) => {
     setTodos(completedList);
   };
 
+  const editHandler = (e) => {
+    setEditText(e.target.value);
+  };
+
+  const editFormHandler = (e) => {
+    e.preventDefault();
+    let editedList = todos.map((todo) => {
+      if (todo.id === item.id) {
+        return { ...todo, title: editText };
+      }
+      return todo;
+    });
+    setTodos(editedList);
+    setEdit(!edit);
+  };
+
   return (
     <div className="todo">
-      <p>Todo: {item.title}</p>
-      <p>complete: {item.complete ? "yes" : "no"}</p>
-      <button onClick={deleteHandler}>Delete</button>
-      <button onClick={completeHandler}>update</button>
+      <p className="todo-title">{item.title}</p>
+      <p className="todo-complete">complete: {item.complete ? "yes" : "no"}</p>
+      <div className="todo-options">
+        <button onClick={deleteHandler}>Delete</button>
+        <button onClick={completeHandler}>Complete</button>
+        <button
+          onClick={() => {
+            setEdit(!edit);
+          }}
+        >
+          Edit
+        </button>
+      </div>
+      {/* if you hit edit this input pops up to edit todo title */}
+      {edit ? (
+        <form className="edit-form" onSubmit={editFormHandler}>
+          <input onChange={editHandler} value={editText} />
+          <button type="submit">Update Todo</button>
+        </form>
+      ) : null}
     </div>
   );
 };
