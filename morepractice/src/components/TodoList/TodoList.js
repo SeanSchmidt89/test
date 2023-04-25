ab541166d6d96ce6d8dacf707bdaeb03fe24b48f
sean <seanschmidt89@yahoo.com>
2023-04-25 01:17:31 -0400

    adding css to all comps

--- morepractice/src/components/TodoList/TodoList.js
+++ morepractice/src/components/TodoList/TodoList.js
@@ -8,6 +8,16 @@ const TodoList = () => {
   return (
     <div className="todo-list">
       <h2>TodoList</h2>
+      {data.length > 0 && (
+        <div className="filter">
+          <p>Filter Priorrity:</p>
+          <select defaultValue="-">
+            <option>-</option>
+            <option>High</option>
+            <option>Low</option>
+          </select>
+        </div>
+      )}
       {data.length > 0 &&
         data.map((item) => <TodoItem key={item.id} item={item} />)}
     </div>
