43a37ba6d844689766fccb24e0ae133198753afc
sean <seanschmidt89@yahoo.com>
2023-09-16 18:07:39 -0400

    adding comps

--- crud-todo-practice/src/App.js
+++ crud-todo-practice/src/App.js
@@ -1,12 +1,10 @@
 import "./App.css";
 import Home from "./comps/Home/Home";
-import Form from "./comps/Form/Form";
 
 function App() {
   return (
     <div className="App">
       <Home />
-      <Form />
     </div>
   );
 }
--- crud-todo-practice/src/comps/Home/Home.js
+++ crud-todo-practice/src/comps/Home/Home.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Form from "../Form/Form";
+import List from "../List/List";
 import "./Home.css";
 
 const Home = () => {
   return (
     <div className="home">
       <h1>Seans Todo List</h1>
+      <Form />
+      <List />
     </div>
   );
 };
